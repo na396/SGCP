8c8fbed1aaf3c47a8a745d6a1b15f3fc530577e6
na396 <niloofar.abiane@gmail.cm>
2022-11-17 23:56:48 -0500

    wind2rep3

--- R/globals.R
+++ R/globals.R
@@ -2,4 +2,4 @@ utils::globalVariables(c("GOtype", "Method", "Pvalue", "Var1", "Var2",
                         "annotation_db", "clusterLabel", "clusterNum",
                         "conductVal", "firstOrder", "index", "kfirst",
                         "ksecond", "lb", "logPvalue", "remains",
-                        "secondOrder", "silIndex", "value"))
+                        "secondOrder", "silIndex", "value", "clusterLabels"))
